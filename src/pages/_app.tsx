ce9364d7dbfa81ff724ea8c013352be3c67155fe
JonathSilva <jonathansilvadacostalima@gmail.com>
2022-04-06 00:27:31 -0300

    fix(public): change in favicon and manifest.json

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -16,7 +16,7 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>EEZY.DEV</title>
+        <title>EEZY.DEV | Portfolio</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
