d34f3cab1dcd7183643c474b2f8b6c72f2e34b41
Jonathan Silva <jonathansilvadacostalima@gmail.com>
2022-08-27 13:44:19 -0300

    Update index.tsx

--- src/components/AboutMe/index.tsx
+++ src/components/AboutMe/index.tsx
@@ -6,7 +6,7 @@ export const AboutMe = () => {
       <h2 data-aos="fade-up">{'About Me'}</h2>
 
       <p data-aos="fade-left">
-        Developer with focus on Mobile and Games, passionate about cinema,
+        Developer with focus on Back-end and Games, passionate about cinema,
         photography and video games, my professional goal is to work with games
         in the future, I also draw conceptual arts using digital painting and 3D
         modeling, but I take it as a hobby.
