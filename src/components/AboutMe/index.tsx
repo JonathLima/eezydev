b7a1b9c06f63b7fd44928d6d0a324906b1e3a2ca
Jonathan Silva <jonathansilvadacostalima@gmail.com>
2023-06-13 00:08:10 -0300

    refactor: alter about me text

--- src/components/AboutMe/index.tsx
+++ src/components/AboutMe/index.tsx
@@ -1,19 +1,19 @@
-import * as S from './styles'
+import * as S from './styles';
 
 export const AboutMe = () => {
   return (
     <>
       <S.AboutContainer id="About">
-        <h2 data-aos="fade-up">{'About Me'}</h2>
+        <h2 data-aos="fade-up">{"About Me"}</h2>
 
         <p data-aos="fade-left">
-          Back-end and mobile developer with a passion for film, photography and
-          games, my professional goal is to create immersive and captivating
+          Mobile and Front-end developer with a passion for film, photography and games,
+          my professional goal is to create immersive and captivating
           experiences in the gaming world. I am diving head-first into 3D
           Animation and Game design to take my projects to a new level of
           creativity and innovation.
         </p>
       </S.AboutContainer>
     </>
-  )
+  );
 }
